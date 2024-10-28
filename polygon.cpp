8185f8f3c9ada9d93e3aa0481d2057372924f303
Kang-Ding-Xin-NYCU <kang313552040.cs13@nycu.edu.tw>
2024-10-28 09:23:57 +0800

    change datastructure

--- polygon.cpp
+++ polygon.cpp
@@ -1,35 +1,96 @@
-#include"polygon.hpp"
-#include<iostream>
+#include "polygon.hpp"
 
-void polygon::input(std::queue<float> in_x, std::queue<float> in_y){
-    x = in_x;
-    y = in_y;
+polygon::polygon()
+{
+
 }
 
-void polygon::print(){
-    std::queue<float> printx = x;
-    std::queue<float> printy = y;
-    for(int i = 0; i < x.size(); i++){
-        std::cout<<"("<< printx.front()<<","<<printy.front()<<")";
-        printx.pop();
-        printy.pop();
+polygon::polygon(std::vector<double> in_x, std::vector<double> in_y)
+{
+    datax = in_x;
+    datay = in_y;
+}
+
+polygon::~polygon()
+{
+    datax.clear();
+    datay.clear();
+}
+
+polygon & polygon::operator=(polygon const & other){
+    for(int i = 0; i < datax.size(); ++i){
+        datax.push_back(other.x(i));
+        datay.push_back(other.y(i));
     }
-    std::cout<<"\n";
+    return *this;
 }
 
-float polygon::area(){
-    float sum = 0;
-    std::queue<float> areax = x;
-    std::queue<float> areay = y;
-    float x1, x2, y1, y2;
-    x1 = areax.front(); areax.push(x1); areax.pop();
-    y1 = areay.front(); areay.push(y1); areay.pop();
-    for(int i = 0; i < x.size(); i++){
-        x2 = areax.front(); areax.push(x2); areax.pop();
-        y2 = areay.front(); areay.push(y2); areay.pop();
-        sum += x1 * y2 - x2 * y1;
-        x1 = x2;
-        y1 = y2;
+double const & polygon::x(std::size_t t) const{
+    if(t > datax.size() - 1){
+        throw std::out_of_range("Line index out of range");
+    }
+    return datax[t];
+}
+
+double & polygon::x(std::size_t t){
+    if(t > datax.size() - 1){
+        throw std::out_of_range("Line index out of range");
+    }
+    return datax[t];
+}
+
+double const & polygon::y(std::size_t t) const{
+    if(t > datay.size() - 1){
+        throw std::out_of_range("Line index out of range");
+    }
+    return datay[t];
+}
+
+double & polygon::y(std::size_t t){
+    if(t > datay.size() - 1){
+        throw std::out_of_range("Line index out of range");
+    }
+    return datay[t];
+}
+
+void polygon::set_x(std::vector<double> in_x)
+{
+    datax = in_x;
+}
+
+std::vector<double> polygon::get_x()
+{
+    return datax;
+}
+
+void polygon::set_y(std::vector<double> in_y)
+{
+    datay = in_y;
+}
+
+std::vector<double> polygon::get_y()
+{
+    return datay;
+}
+
+void polygon::print()
+{
+    for(size_t i = 0; i < datax.size(); ++i)
+    {
+        std::cout << "(" << datax[i] << "," << datay[i] << ")";
+    }
+    std::cout << "\n";
+}
+
+double polygon::area()
+{
+    double sum = 0;
+    std::vector<double> areax = datax;
+    std::vector<double> areay = datay;
+    for(int i = 0; i <= datax.size(); ++i)
+    {
+        sum += datax[i] * datay[i + 1] - datax[i + 1] * datay[i];
     }
     return sum/2;
 }
+
--- polygon.hpp
+++ polygon.hpp
@@ -1,16 +1,28 @@
-#include<queue>
+#include <vector>
+#include <iostream>
 
 #ifndef POLYGON
 #define POLYGON
 
 class polygon {
 public:
-    void input(std::queue<float> in_x, std::queue<float> in_y);
-    void print();
-    float area();
+    polygon();
+    polygon(std::vector<double> in_x, std::vector<double> in_y);
+    ~polygon();
+    polygon & operator=(polygon const & );
+    double const & x(std::size_t it) const;
+    double       & x(std::size_t it);
+    double const & y(std::size_t it) const;
+    double       & y(std::size_t it);
+    void                set_x(std::vector<double> in_x);
+    std::vector<double> get_x();
+    void                set_y(std::vector<double> in_y);
+    std::vector<double> get_y();
+    void                print();
+    double              area();
 
 private:
-    std::queue<float> x, y;
+    std::vector<double> datax, datay;
 };
 
 #endif
