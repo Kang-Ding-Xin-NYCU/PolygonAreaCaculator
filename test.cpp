4e2e56d150a00dd6b1fa9655d9e7c11896c65786
Kang-Ding-Xin-NYCU <kang313552040.cs13@nycu.edu.tw>
2024-11-02 23:06:42 +0800

    fix simple test file

--- test.cpp
+++ test.cpp
@@ -1,23 +1,24 @@
 #include "_polygon.hpp"
 
 int main(){
-    std::vector<double> a;
+    std::vector<std::vector<double>> a;
     std::vector<double> b;
-    a.push_back(0);
-    a.push_back(3);
-    a.push_back(3);
-    a.push_back(0);
-    b.push_back(0);
-    b.push_back(0);
-    b.push_back(3);
-    b.push_back(3);
+    b.resize(2);
+    b[0] = 0; b[1] = 0;
+    a.push_back(b);
+    b[0] = 0; b[1] = 3;
+    a.push_back(b);
+    b[0] = 3; b[1] = 3;
+    a.push_back(b);
+    b[0] = 3; b[1] = 0;
+    a.push_back(b);
     polygon A;
-    A.set_x(a);
-    A.set_y(b);
-    A.print();
+    A.polygon::set(a);
+    A.polygon::print();
     polygon B = A;
     B.print();
     std::cout << A.area() << "\n";
-    std::cout << B.area() << "\n"; 
+    std::cout << B.area() << "\n";
+     
     return 0;
 }
