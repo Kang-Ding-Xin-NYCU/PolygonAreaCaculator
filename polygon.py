58093bdfe2d1b089c38e3eb3a188ddd842547791
Kang-Ding-Xin-NYCU <kang313552040.cs13@nycu.edu.tw>
2024-12-14 22:02:09 +0800

    revise file

--- polygon.py
+++ polygon.py
@@ -50,14 +50,3 @@ class polygon:
             print(f"Failed to save image: {filename}")
         else:
             print(f"Image saved to: {filename}")
-
-    '''
-    def plot(self):
-        img = np.zeros((300,300,3), dtype='uint8')
-        points = np.array(self.polygon.get_vertices(), dtype=np.int32)
-        points = points.reshape((-1, 1, 2))
-        cv2.polylines(img, [points], True, (0, 0, 255), 5)
-        cv2.imshow('polygon', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        '''
